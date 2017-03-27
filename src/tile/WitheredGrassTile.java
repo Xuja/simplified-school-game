779301766f959c2d367db9502f4406478be8ce5b
Xu <xwjl92@hotmail.com>
2017-03-27 16:34:29 +0200

    grass

---
+++ src/tile/WitheredGrassTile.java
@@ -0,0 +1,10 @@
+package tile;
+
+public class WitheredGrassTile extends Tiles{
+
+	public WitheredGrassTile(String tileTexture, int id) {
+		super(tileTexture, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
