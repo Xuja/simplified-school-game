7acc7013bb09d7010cd2d4fce5525c0518847f42
Xu <xwjl92@hotmail.com>
2017-04-04 12:10:12 +0200

    forgot something

--- src/entity/EntityState.java
+++ src/entity/EntityState.java
@@ -8,11 +8,11 @@ public class EntityState {
 
 	private static HashMap<String,EntityState> stateMap = new HashMap<String,EntityState>();
 	
-	public static final EntityState IDLE = new EntityState("res/textures/player/DOWN1.png");
-	public static final EntityState IDLE_UP = new EntityState("res/textures/player/UP1.png");
-	public static final EntityState IDLE_DOWN = new EntityState("res/textures/player/DOWN1.png");
-	public static final EntityState IDLE_LEFT = new EntityState("res/textures/player/LEFT1.png");
-	public static final EntityState IDLE_RIGHT = new EntityState("res/textures/player/RIGHT1.png");
+	public static final EntityState IDLE = new EntityState("idle");
+	public static final EntityState IDLE_UP = new EntityState("idle_up");
+	public static final EntityState IDLE_DOWN = new EntityState("idle_down");
+	public static final EntityState IDLE_LEFT = new EntityState("idle_left");
+	public static final EntityState IDLE_RIGHT = new EntityState("idle_right");
 	public static final EntityState WALK_UP = new EntityState("walk_up"); //hoe doe ik hier meer images achter elkaar?
 	public static final EntityState WALK_DOWN = new EntityState("walk_down");
 	public static final EntityState WALK_LEFT = new EntityState("walk_left");
