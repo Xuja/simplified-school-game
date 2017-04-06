4f16c924d7add1c5a5c2012e7103c85662ff8f45
Robin <robinvd95@live.nl>
2017-04-06 14:21:22 +0200

    Removed unused import

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,14 +1,5 @@
 package entity;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 import game.EnumDirection;
 import input.IActionListener;
 import input.InputAction;
